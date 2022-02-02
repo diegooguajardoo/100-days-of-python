95768ad803852b72f728cfdc97067c7dcee8a4fd
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-02-02 14:27:33 -0600

    update def

--- grader ch4.py
+++ grader ch4.py
@@ -1,26 +1,22 @@
 def compute(sc):
-	if sc <= 1:
-		if sc >= .9:
-			grade = "A"
-		elif sc >=.8:
-			grade = "B"
-		elif sc >=.7:
-			grade = "C"
-		elif sc >=.6:
-			grade = "D"
-		elif sc <.6:
-			grade = "F"
-		return grade
-	else:
-		print("Type a number between 0 and 1 please")
+	try:
+		float(sc) or sc >1
+		if sc <= 1:
+			if sc >= .9:
+				grade = "A"
+			elif sc >=.8:
+				grade = "B"
+			elif sc >=.7:
+				grade = "C"
+			elif sc >=.6:
+				grade = "D"
+			elif sc <.6:
+				grade = "F"
+			print ("Your grade is:", grade)
+			return grade
+	except:
+		print("Bad score")
 
 
 score = input("What is the score? \n")
-
-try:
-	score = float(score)
-	total = compute(score)
-	print ("Your grade is:", total)
-except:
-	print("Enter a digit please")
-	
+total = compute(score)
