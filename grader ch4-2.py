ac809a953d9474e5ca097cb58b9ecff09f625c39
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-02-02 14:37:27 -0600

    create file

---
+++ grader ch4-2.py
@@ -0,0 +1,21 @@
+def compute(sc):
+	try:
+		float(sc) or sc >1 #si es numero o mas de 1
+		if sc <= 1:
+			if sc >= .9:
+				grade = "A"
+			elif sc >=.8:
+				grade = "B"
+			elif sc >=.7:
+				grade = "C"
+			elif sc >=.6:
+				grade = "D"
+			elif sc <.6:
+				grade = "F"
+			print ("Your grade is:", grade)
+			return grade
+	except:
+		print("Bad score") #todo tipo de bad score
+
+total = compute(score)
+score = input("What is the score? \n")
