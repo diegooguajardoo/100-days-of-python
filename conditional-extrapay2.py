01d0895f7e026abab990f6689fbf84b1845424d0
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-02-02 09:16:43 -0600

    wenseday

--- conditional-extrapay2.py
+++ conditional-extrapay2.py
@@ -1,22 +1,23 @@
-hrs = input("Enter hours: ")
-regularrate = input("Enter rate: ")
-
-def computepay():
-	hrs = float(hrs)
-	regularrate = float(regularrate)
-	pay = hrs * regularrate
+def computepay(aliash, aliasr):
+	if aliash <= 40:
+		pay = aliash * aliasr
+		print(pay)
+	else:
+		extrahrs = aliash - 40
+		regpay = 40 * aliasr
+		extrapay = extrahrs * aliasr * 1.5
+		pay = regpay + extrapay
+		print(pay)
 	return pay	
 
-extrarate = regularrate * 1.5
-if hrs <= 40:
-	pay = hrs * regularrate
-	print(pay)
-else:
-	extrahrs = hrs - 40
-	pay = 40 * regularrate
-	extrapay = extrahrs * extrarate
-	pay = pay + extrapay
-	print(pay)
+floathrs = float(input("Enter hours: "))
+floatregularrate = float(input("Enter rate: "))
+
+p = computepay(floathrs, floatregularrate)
+#pay = hrs * regularrate
+
+
+
     #if extrahrs > 1:
     #    print("you got payed",extrahrs,"extra hours!")
     #else:
