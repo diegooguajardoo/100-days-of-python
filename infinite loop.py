7c93c2c5f73e8aa47b57333dc7c02a2407591d0b
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-02-04 21:10:55 -0600

    commit add comment

--- infinite loop.py
+++ infinite loop.py
@@ -1,7 +1,7 @@
 import random
 
 print("Select difficulty level: easy, medium, hard")
-difficulty = input("How hard do you want the level to be?")
+difficulty = input("How hard do you want the level to be?\n")
 
 difficulty.lower() == ["easy", "medium", "hard"]
 if difficulty == "easy":
@@ -13,17 +13,27 @@ else:
 
 random_number = random.randint(0, top_of_range)
 guesses = 0
-print ("Random number is", random_number)
+
 while True:
-	user_guess = int(input("Make a guess"))
 	guesses += 1
+	user_guess = input("Make a guess: ")
+	if user_guess.isdigit():
+		user_guess = int(user_guess)
+	else:
+		print("Please type a number next time)")
+		continue #this takes the user to the top of the loop if it is not a digit, therefore asking again for a digit guess
+
 	if user_guess == random_number:
 		print("Right!")
 		break
-	elif user_guess > top_of_range:
-		print("Make it smaller")
 	else:
-		print("Make it higher, take note of the difficulty")
-	continue
+		print("you got it wrong!")
+#	elif user_guess > top_of_range:
+#		print("Make it smaller\n")
+#	else:
+#		print("Make it higher\n")
+#	continue	
+#
 
 print("you got it in", guesses, "guesses")
+print(random_number)
