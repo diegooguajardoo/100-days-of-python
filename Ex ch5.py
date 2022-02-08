12b3647f8fb296194bf19fe9ecc90505b52eecec
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-02-07 21:28:42 -0600

    review code

--- Ex ch5.py
+++ Ex ch5.py
@@ -5,9 +5,6 @@ average = 0
 while True:
 	number = input("Enter a number: ")
 	if number == "done":
-		print("Total is: ",total)
-		print("Count is: ",cnt)
-		print("Average is: ",average)
 		break
 	try:
 		number = int(number)
@@ -17,3 +14,5 @@ while True:
 	cnt = cnt + 1
 	total = number + total
 	average = total / cnt
+
+print(total, cnt, average)
--- ex5-3.py
+++ ex5-3.py
@@ -1,12 +1,25 @@
 largest = None
 smallest = None
-while True:
-    num = input("Enter a number: ")
-    if num == "done":
-        break
-    print(num)
-	elif num > largest:
-		largest = num
 
+while True:
+	num = input("Enter a number: ")
+	if num == "done":
+		break
+	try:
+		num = int(num)
+	except:
+		print("Invalid input")
+		continue
+	if largest == None:
+		largest = num
+	if smallest == None:
+		smallest = num
+	
+	if num > largest:
+		largest = num
+	elif num < smallest:
+		smallest = num
+	
 
 print("Maximum", largest)
+print("Minimun", smallest)
