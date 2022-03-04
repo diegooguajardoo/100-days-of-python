9e65028e53b6b608779c4fe15614551f56a1af40
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-03-03 23:03:35 -0600

    update elifs

--- Personal Projects/BMI2.py
+++ Personal Projects/BMI2.py
@@ -1,4 +1,3 @@
-
 # 🚨 Don't change the code below 👇
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
@@ -12,8 +11,9 @@ if bmi < 18.5:
   print(f"Your BMI is {bmi}, you are underweight.")
 elif bmi < 25:
   print(f"Your BMI is {bmi}, you have a normal weight.")
-elif bmi > 25 and bmi <30:
-  print
-elif bmi > 30:
-
-
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print("Your BMI is {bmi}, are clinically obese")
