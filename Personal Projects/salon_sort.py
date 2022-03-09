f2c42f2512d7033dfe67c40a32dc1b6eee31a24b
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-03-09 13:22:15 -0600

    full iteration

--- Personal Projects/salon_sort.py
+++ Personal Projects/salon_sort.py
@@ -1,14 +1,16 @@
 place = 0
+count = 0
 character = ["a", "b", "c"] 
-salon = ["Regina Aguirre Corvera", "Juan Carlos Arguelles Jaramillo", "Eugenia Berlanga García", "Regina Cabada Aguirre", "Monteserrat Cantú Silva", "Valentina Alejandra GuadalupeDurán Lozano", "Danna Paola Galarza López", "Sofía Daniela Garza Villarreal", "Lizbeth Valeria González Sepúlveda", "Gustavo Emanuel Guerra Gómez", "Verónica Iga Gutiérrez", "Sara López Quiroz", "André Millán Rivera",
+lista = ["Regina Aguirre Corvera", "Juan Carlos Arguelles Jaramillo", "Eugenia Berlanga García", "Regina Cabada Aguirre", "Monteserrat Cantú Silva", "Valentina Alejandra GuadalupeDurán Lozano", "Danna Paola Galarza López", "Sofía Daniela Garza Villarreal", "Lizbeth Valeria González Sepúlveda", "Gustavo Emanuel Guerra Gómez", "Verónica Iga Gutiérrez", "Sara López Quiroz", "André Millán Rivera",
          "Marcela Morales De la Garza", "Ana Paula Morales González", "Mónica Daniela Pedraza Galván", "María Fernanda Pérez Castillo", "Diego Alejandro Pérez Frías", "Maximiliano Roca Cavazos", "César Zahid Rosales", "María Fernanda Ruelas Reza", "Jorge Salazar Gaona", "Paulina Solis Cruz", "Isabella Tamez Cárdenas", "David Melchidek Tompkins Gómez", "Melissa Treviño García", "Luis Gabriel Zambrano Kunte"]
-for letter in salon[place]:
-	try:
-		if letter == "l":
-			print(f"Result: '{letter}' found in {place}")
-			break
-		else:
-			place += 1
-	except:
-		print("Not found anywhere.")
+
+for person in lista:
+	count += 1
+	for character in person:
+		print(character)
+
+	if character == "a":
+		print(f"Result: '{character}' found in {place}")
+	else:
+		print(f"Not found anywhere {count}.")
 
