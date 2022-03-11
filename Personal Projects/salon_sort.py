3ab1969ed898cde6f18bb2c6340f15bcee66c24f
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-03-10 23:22:54 -0600

    update variables and results

--- Personal Projects/salon_sort.py
+++ Personal Projects/salon_sort.py
@@ -2,12 +2,12 @@ import itertools
 #place = 0
 alumnos = 0
 totalcaracteresind = 0
-totalcaracteres = 0
-abcs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "s"] 
+abcs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
 letteriter = -1
 linecount = 0
+chartot = 0
 lista = ["Regina Aguirre Corvera", "Juan Carlos Arguelles Jaramillo", "Eugenia Berlanga García", "Regina Cabada Aguirre", "Monteserrat Cantú Silva", "Valentina Alejandra Guadalupe Durán Lozano", "Danna Paola Galarza López", "Sofía Daniela Garza Villarreal", "Lizbeth Valeria González Sepúlveda", "Gustavo Emanuel Guerra Gómez", "Verónica Iga Gutiérrez", "Sara López Quiroz", "André Millán Rivera",
-         "Marcela Morales De la Garza", "Ana Paula Morales González", "Mónica Daniela Pedraza Galván", "María Fernanda Pérez Castillo", "Diego Alejandro Pérez Frías", "Maximiliano Roca Cavazos", "César Zahid Rosales", "María Fernanda Ruelas Reza", "Jorge Salazar Gaona", "Paulina Solis Cruz", "Isabella Tamez Cárdenas", "David Melchidek Tompkins Gómez", "Melissa Treviño García", "Luis Gabriel Zambrano Kuntes"]
+         "Marcela Morales De la Garza", "Ana Paula Morales González", "Mónica Daniela Pedraza Galván", "María Fernanda Pérez Castillo", "Diego Alejandro Pérez Frías", "Maximiliano Roca Cavazos", "César Zahid Rosales", "María Fernanda Ruelas Reza", "Jorge Salazar Gaona", "Paulina Solis Cruz", "Isabella Tamez Cárdenas", "David Melchidek Tompkins Gómez", "Melissa Treviño García", "Luis Gabriel Zambrano Kunte"]
 
 for i in abcs:
 	letteriter += 1
@@ -17,12 +17,12 @@ for i in abcs:
 	
 	for person in lista:
 		totalcaracteresind = 0
-		
+		if letteriter == 0:
+			chartot = len(person) + chartot
 
 
 		for sample in person:
 			totalcaracteresind += 1
-			totalcaracteres += 1
 			if sample == abcs[letteriter]:
 				print(f"Result: '{sample}' found in {totalcaracteresind}th letter of {person} ")
 				resultcount += 1
@@ -42,9 +42,8 @@ for i in abcs:
 #Resultados al final
 print(f"Se analizaron {len(abcs)} letras.")
 print(f"La lista de letras buscadas son: {abcs}.")
-
 print(f"Hay {len(lista)} alumnos en la lista.")
-print(f"Hay {totalcaracteres} caracteres en total.")
+print(f"Hay {chartot} caracteres en total.")
 #print(f'Final Result: "{abcs[0]}" se encontró {resultcount} veces en total.')
 print(f"Se iteró la búsqueda {linecount} veces.")
 	
