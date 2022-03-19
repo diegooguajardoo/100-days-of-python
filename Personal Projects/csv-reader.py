7341e7b0fdc5a4eb4f5fe4f975a15eca217687ad
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-03-18 20:23:46 -0600

    create csvlist

--- Personal Projects/csv-reader.py
+++ Personal Projects/csv-reader.py
@@ -2,5 +2,11 @@ from csv import reader
 
 with open("lista202.csv", "r") as read_obj:
 	csv_reader = reader(read_obj)
-	list_of_rows = list(csv_reader)
-	print(list_of_rows)
+	documento = list(csv_reader)
+
+row_number = 2
+col_number = 1
+documento[row_number - 1][col_number - 1] = "Re"
+value = documento[row_number - 1][col_number - 1]
+
+print('Value in cell at 3rd row and 2nd column : ', value)
