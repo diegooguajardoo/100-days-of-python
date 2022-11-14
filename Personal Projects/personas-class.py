36bf7a82bd788b34a3aed8112f8026b762aba264
Diego Guajardo Gómez <97126264+diegooguajardoo@users.noreply.github.com>
2022-11-14 09:51:49 -0600

    files added pandas training complete

---
+++ Personal Projects/personas-class.py
@@ -0,0 +1,6 @@
+class Persona():
+	def __init__(self) -> None:
+		self.character_lenght = 0
+		self.nombre = self.text()
+		self.single_letter_search_result = ""
+		self.syllable_search_result = ""
--- Personal Projects/salon_sort.py
+++ Personal Projects/salon_sort.py
@@ -1,3 +1,5 @@
+from listadata import lista, lista2, listastr
+
 n = 0
 totalcaracteresind = 0
 abcs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
@@ -14,11 +16,6 @@ personasunicas = []
 silabasunicas = []
 resultcount = 0
 
-lista = ["Regina Aguirre Corvera", "Juan Carlos Arguelles Jaramillo", "Eugenia Berlanga García", "Regina Cabada Aguirre", "Monteserrat Cantú Silva", "Valentina Alejandra Guadalupe Durán Lozano", "Danna Paola Galarza López", "Sofía Daniela Garza Villarreal", "Lizbeth Valeria González Sepúlveda", "Gustavo Emanuel Guerra Gómez", "Verónica Iga Gutiérrez", "Sara López Quiroz", "André Millán Rivera",
-         "Marcela Morales De la Garza", "Ana Paula Morales González", "Mónica Daniela Pedraza Galván", "María Fernanda Pérez Castillo", "Diego Alejandro Pérez Frías", "Maximiliano Roca Cavazos", "César Zahid Rosales", "María Fernanda Ruelas Reza", "Jorge Salazar Gaona", "Paulina Solis Cruz", "Isabella Tamez Cárdenas", "David Melchidek Tompkins Gómez", "Melissa Treviño García", "Luis Gabriel Zambrano Kunte"]
-lista2 = ["Victoria Aguayo Sepúlveda","Amelie Balderrama Ramírez","Anabella Bueno","Geovani Gamaliel Camarillo Castro","María Fernanda Castillo Navarro","Mariangela De León Martínez","Jesús Gerardo De León Rivera","Alejandro Elizondo Peña","Jimena Flores","Renata García Morales","Eunise Victoria González Villarreal","Emilio González Álvarez","Ángela Regina Gutiérrez Arredondo","Nicole Dominick Jaubert Martínez","Jimena Lara Leija","Daniel de Jesús Maldonado Rodríguez","Salvador Muñoz Lavin","Daniela Ortega Martínez","Narcedalia Perales Lozano","Jimena María Peña Treviño","Valeria Pérez Estrada","Mariana Quiroz Hernández","Barbara Rodríguez de la Torre","Eugenia Ruiz Ransom","Mariangela Astrea Salinas Flores","Carlos Alonso Serroque Palacios","Paulina Silva Rivera"]
-listastr = ["Regina Aguirre Corvera", "Juan Carlos Arguelles Jaramillo", "Eugenia Berlanga García", "Regina Cabada Aguirre", "Monteserrat Cantú Silva", "Valentina Alejandra Guadalupe Durán Lozano", "Danna Paola Galarza López", "Sofía Daniela Garza Villarreal", "Lizbeth Valeria González Sepúlveda", "Gustavo Emanuel Guerra Gómez", "Verónica Iga Gutiérrez", "Sara López Quiroz", "André Millán Rivera",
-            "Marcela Morales De la Garza", "Ana Paula Morales González", "Mónica Daniela Pedraza Galván", "María Fernanda Pérez Castillo", "Diego Alejandro Pérez Frías", "Maximiliano Roca Cavazos", "César Zahid Rosales", "María Fernanda Ruelas Reza", "Jorge Salazar Gaona", "Paulina Solis Cruz", "Isabella Tamez Cárdenas", "David Melchidek Tompkins Gómez", "Melissa Treviño García", "Luis Gabriel Zambrano Kunte"]
 initiallen = len(lista)
 
 #SINGLE SEARCH
